69d1eb662eda889d8eb84f517ca4021a8dac956c
bayupamuji <bayuteguhpamuji@gmail.com>
2023-02-28 09:26:15 +0700

    create list item & implement lazy layout

--- app/src/main/java/com/example/jetheroes/JetHeroesApp.kt
+++ app/src/main/java/com/example/jetheroes/JetHeroesApp.kt
@@ -1,16 +1,61 @@
 package com.example.jetheroes
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.jetheroes.model.HeroesData
 import com.example.jetheroes.ui.theme.JetHeroesTheme
 
 @Composable
 fun JetHeroesApp(modifier: Modifier = Modifier) {
     Box(modifier = modifier) {
+        LazyColumn {
+            items(HeroesData.heroes, key = { it.id }) { hero ->
+                HeroListItem(
+                    name = hero.name,
+                    imgUrl = hero.imgUrl,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
 
+@Composable
+fun HeroListItem(name: String, imgUrl: String, modifier: Modifier = Modifier) {
+    Row(verticalAlignment = Alignment.CenterVertically, modifier = modifier.clickable { }) {
+        AsyncImage(
+            model = imgUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(8.dp)
+                .size(60.dp)
+                .clip(
+                    CircleShape
+                )
+        )
+        Text(
+            text = name,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(16.dp)
+        )
     }
 }
 
@@ -20,4 +65,13 @@ fun JetHeroAppPreview() {
     JetHeroesTheme {
         JetHeroesApp()
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HeroItemPrev() {
+    HeroListItem(
+        name = "KH. Akhmad Dahlan",
+        imgUrl = "https://raw.githubusercontent.com/dicodingacademy/assets/main/android_compose_academy/pahlawan/11.jpg"
+    )
 }
